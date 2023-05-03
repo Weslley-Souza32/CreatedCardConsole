e669ce7721d19b99a6d049053a973d2f413c87df
Weslley Souza <weslley.souza@soupromove.com.br>
2023-05-03 16:01:53 -0300

    Second commit

--- CreatedCardConsole/Program.cs
+++ CreatedCardConsole/Program.cs
@@ -2,7 +2,7 @@
 using CreatedCardConsole.Services;
 
 CardService service =  new CardService();
-Responsible responsible = new Responsible(0, "Weslley Junio", "09597331675");
+Responsible responsible = new Responsible(0, "Weslley Junio", "000.000.000-00");
 
 Card myCard = CreateCard.CreatedCard(Brand.MasterCard, responsible);
 myCard.Deposit(1000);
